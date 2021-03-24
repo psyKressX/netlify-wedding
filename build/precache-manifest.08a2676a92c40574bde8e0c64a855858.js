7ed072e9948a69f692b7ebbce2ceaaeba3c931d4
liam smith <liam_smith1989@hotmail.com>
2021-03-24 16:24:46 +1100

    6 commit

---
+++ build/precache-manifest.08a2676a92c40574bde8e0c64a855858.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ce567085dc28e3f6abbf90d5ec6fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a335c1209ded9fdef47",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "6df9cf146f4443f3d8b0",
+    "url": "/static/css/main.78cb465d.chunk.css"
+  },
+  {
+    "revision": "6a335c1209ded9fdef47",
+    "url": "/static/js/2.1b04b0f8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1b04b0f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df9cf146f4443f3d8b0",
+    "url": "/static/js/main.018c06ed.chunk.js"
+  },
+  {
+    "revision": "4da4107eec20fab9f442",
+    "url": "/static/js/runtime-main.cc51c47b.js"
+  },
+  {
+    "revision": "ed5ec0907ad0ae05139de910a564420b",
+    "url": "/static/media/Cinzel.ed5ec090.ttf"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/static/media/Quicksand.f9baef8a.ttf"
+  },
+  {
+    "revision": "d05251c6f3b6cd35c163e52c4530e987",
+    "url": "/static/media/thumbnail_Image.d05251c6.jpg"
+  }
+]);
