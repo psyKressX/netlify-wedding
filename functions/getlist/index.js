0eb677efd0e4a14d65a780d10e8774db274ee3ad
liam smith <liam_smith1989@hotmail.com>
2021-03-25 14:02:23 +1100

    13

--- functions/getlist/index.js
+++ functions/getlist/index.js
@@ -1,33 +1,24 @@
 require("dotenv").config();
-const knex = require("knex");
-const knexDB = knex({
-  client: "pg",
-  connection: {
-    host: process.env.DB_URL,
-    database: "wedding",
-    user: "liam",
-  },
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.SECRET,
 });
 
-exports.handler = async (req, context, callback) => {
-  let status, res;
-  await knexDB("guest_list")
-    .select("*")
-    .then(async (data1) => {
-      let promises = data1.map((p) => knexDB("others").where({ id: p.id }));
-      const data2 = await Promise.all(promises);
-      for (let i = 0; i < data1.length; i++) {
-        data1[i].others = data2[i];
-      }
-      res = data1;
-      console.log("hello", res);
-    })
-    .catch((err) => console.log(err));
-  await knexDB.destroy().then(() => {
-    console.log("destroyed");
-    callback(null, {
+exports.handler = async (req, context) => {
+  try {
+    const res = await client.query(q.Paginate(q.Match(q.Index("guests"))));
+    console.log(res);
+    return {
       statusCode: 200,
-      body: JSON.stringify(res),
-    });
-  });
+      body: res,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 400,
+      body: "err",
+    };
+  }
 };
