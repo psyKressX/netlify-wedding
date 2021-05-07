54e790faf8dde3bce24ba096abc213bfb625a627
psyKressX <50396417+psyKressX@users.noreply.github.com>
2021-05-07 10:54:21 +1000

    Update index.js

--- functions/getlist/index.js
+++ functions/getlist/index.js
@@ -2,9 +2,14 @@ const faunaDB = require('faunadb')
 const dbName = new faunaDB.Client({
   secret: process.env.SECRET
 })
-
+const res = await client.query(
+      q.Map(
+        q.Paginate(q.Match(q.Index("guests"))),
+        q.Lambda((x) => q.Get(x))
+      )
+    );
 exports.handler = async () => {
-  return dbName.query(faunaDB.query.Paginate(faunaDB.query.Match(faunaDB.query.Index("guests")))).then(indexedData => {
+  return dbName.query(faunaDB.query.Paginate(faunaDB.query.Match(faunaDB.query.Index("guests"))),q.Lambda((x) => q.Get(x))).then(indexedData => {
     const data = indexedData.data.map((i) => i.data);
     return {
       statusCode: 200,
